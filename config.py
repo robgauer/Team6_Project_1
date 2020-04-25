2aea1de331a91866db2847679702730f301fd20f
Rob Gauer <61432095+robgauer@users.noreply.github.com>
2020-04-25 15:54:51 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Enter your API keys
-
-api_key = "6207160404f998769a02f8a9ab406c04a1784af8"
-
-gkey = "AIzaSyDy_9BsWURvh1c5TXZls84NuehshlX2F6U"
-
-openstates = "0b493b7f-47e7-4898-9b02-827e9cea3edf"
